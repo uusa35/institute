006db97068c9d1cb73a88c5ee99b06cd97e0c1e3
uusa35 <uusa35@gmail.com>
2016-10-15 07:45:51 -0400

    some translations

---
+++ resources/lang/ar/messages.php
@@ -0,0 +1,6 @@
+<?php
+
+return array (
+  'allrights' => 'جميع الحقوق محفوظة 2016 ',
+  'designed_developed' => 'تم التطوير والبرمجه بواسطة',
+);
---
+++ resources/lang/en/messages.php
@@ -0,0 +1,6 @@
+<?php
+
+return array (
+  'allrights' => 'all rights reserved 2016',
+  'designed_developed' => 'designed and developed by',
+);
