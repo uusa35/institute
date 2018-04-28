5dd4b63df7c5c137f8ad9b9e8c8f9f9db20a9834
usama ahmed <uusa35@gmail.com>
2018-04-28 11:56:12 +0300

    from my local on mac

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,7 +9,11 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Intervention\Image\Facades\Image;
-
+if (version_compare(PHP_VERSION, '7.2.0', '>=')) {
+    // Ignores notices and reports all other kinds... and warnings
+    error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+    // error_reporting(E_ALL ^ E_WARNING); // Maybe this is enough
+}
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
